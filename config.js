819a5d0777de5846e0d4694be8602fc1341266cc
Agung12 <115615712+agung-bot1@users.noreply.github.com>
2022-10-19 19:41:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
+global.owner = ['6285709420641']
+global.ownernomer = "6285709420641"
+global.premium = ['6285709420641','6285794583615']
+global.packname = 'Agung'
 global.author = 'Cuyy'
 global.sessionName = 'zenss'
 global.prefa = ['','!','.','#','&']
